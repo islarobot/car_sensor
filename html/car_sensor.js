4b62fb3fa36e1eb84bed152dee288df049707463
yagocasasnovas <yago.casasnovas@gmail.com>
2019-01-22 23:15:21 +0100

    s

--- html/car_sensor.js
+++ html/car_sensor.js
@@ -10,8 +10,8 @@ function draw_sector(ctx,x,y,radius,start_a,end_a,radius_delta,value_color,vl) {
 	ctx.beginPath();
 	ctx.arc(x,y,radius,start_a,end_a,false);
 	ctx.arc(x,y,radius-radius_delta,end_a,start_a,true);
-	//ctx.fillStyle = 'green';
-	//ctx.fill();
+	ctx.fillStyle = 'white';
+	ctx.fill();
 	ctx.closePath();
 	ctx.lineWidth = 1;
 	ctx.strokeStyle = 'black';
@@ -21,74 +21,121 @@ function draw_sector(ctx,x,y,radius,start_a,end_a,radius_delta,value_color,vl) {
 }
 
 
-function draw_sector_value(ctx,radius,sector,radius_delta,value_color,vl) {
+function draw_sector_value(ctx,sector,value_color,vl) {
 
 
-
+//console.log("hola");
 	var centerX = vl.w_square / 2;
 	var centerY = vl.h_square / 2;
-
+	
 	//degree to rad
 
 
 	switch(sector) {
   case 0:	
   
-  		start_a = 260;
-  		end_a = 280;
+  		start_a = 250;
+  		end_a = 290;
+  		var delta_y = 0;
+  		var radius = 390;
+  		var radius_delta = 100;
+  		var valor = radius - radius_delta + value_color*2;
+  		
+  		
   		
     break;
   
  case 1:	
   
-  		start_a = 285;
-  		end_a = 305;
+  		start_a = 320;
+  		end_a = 350;
+  		var delta_y = 100;
+  		var radius = 310;
+  		var radius_delta = 100;
+  		var valor = radius - radius_delta + value_color*2;
   		
     break;
     
      case 2:	
   
-  		start_a = 55;
-  		end_a = 75;
+  		start_a = 190;
+  		end_a = 220;
+  		var delta_y = 100;
+  		var radius = 310;
+  		var radius_delta = 100;
+  		var valor = radius - radius_delta + value_color*2;
   		
     break;
     
      case 3:	
   
-  		start_a = 80;
-  		end_a = 100;
+  		start_a = 10;
+  		end_a = 40;
+  		var delta_y = -130;
+  		var radius = 310;
+  		var radius_delta = 100;
+  		var valor = radius - radius_delta + value_color*2;
+  		
   		
     break;
     
      case 4:	
   
-  		start_a = 105;
-  		end_a = 125;
+   	start_a = 70;
+  		end_a = 110;
+  		var delta_y = -30;
+  		var radius = 390;
+  		var radius_delta = 100;
+  		var valor = radius - radius_delta + value_color*2;
+  		
   		
     break;
     
      case 5:	
   
-  		start_a = 235;
-  		end_a = 255;
+  		start_a = 140;
+  		end_a = 170;
+  		var delta_y = -130;
+  		var radius = 310;
+  		var radius_delta = 100;
+  		var valor = radius - radius_delta + value_color*2;
+  		
   		
     break;  
   
 }
+
+//if (sector == 0) {
+///console.log(value_color)
+//console.log(valor)
+//}
 	start_a = 0.0174532925 * start_a;
 	end_a = 0.0174532925 * end_a;
 	
 	ctx.beginPath();
-	ctx.arc(centerX,centerY,radius-1,start_a,end_a,false);
-	ctx.arc(centerX,centerY,radius-radius_delta-1,end_a,start_a,true);
-	//ctx.fillStyle = 'green';
-	//ctx.fill();
+	
+	ctx.arc(centerX,centerY-delta_y,valor-2,start_a,end_a,false);
+	ctx.arc(centerX,centerY-delta_y,radius-radius_delta+2,end_a,start_a,true);
+	ctx.fillStyle = 'green';
+	ctx.fill();
 	ctx.closePath();
 	ctx.lineWidth = 1;
-	ctx.strokeStyle = 'black';
-	ctx.stroke();	
-
-
+	ctx.strokeStyle = 'green';
+	ctx.stroke();
+	
+	ctx.closePath();
+		
+	ctx.beginPath();
+	
+	ctx.arc(centerX,centerY-delta_y,radius-2,start_a,end_a,false);
+	ctx.arc(centerX,centerY-delta_y,valor+2,end_a,start_a,true);
+	ctx.fillStyle = 'white';
+	ctx.fill();
+	ctx.closePath();
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = 'white';
+	ctx.stroke();
+	ctx.closePath();
 }
 
 
